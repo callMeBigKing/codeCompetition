33ae6e5faaba515a4970f1f28ca6fc77a3b96264
BaWang <gosty1@sina.com>
2016-03-13 11:22:01 +0800

    3/13  11:21

--- code/future_net/src/com/routesearch/route/GaEngine.java
+++ code/future_net/src/com/routesearch/route/GaEngine.java
@@ -1,19 +1,34 @@
 package com.routesearch.route;
 
+import java.util.ArrayList;
+import java.util.Queue;
+
 /**
  * Created by imgos on 2016/3/11.
  */
 public class GaEngine {
-    int Popsize; //种群规模
-    int crossRate;//交叉概率
-    int mutationRate;//变异概率
-    int []route;//route[0]存放begin route[end]存放end point  route[1-end]存中间节点
 
+    private int Popsize; //种群规模
+    private int crossRate;//交叉概率
+    private int mutationRate;//变异概率
+    private int []bestRoute;//当前最短路径 route[0]存放begin route[end]存放end point  route[1-end]存中间节点
+    private int graph[][][];
+    //用来存储图，每一行表示一个点可以到达的点和权值，里面存储的是二维数组 [x][0]表示点，[x][1]表示权值.
 
-//    图用稀疏矩阵存储
+    public GaEngine(int [][][] graph){
+//        构造函数传入graph
+        this.graph=graph.clone();
+
+    }
 
     public void  InitPop(){
 //        初始化种群，随机产生一群从起点到终点的路径
+
+    }
+
+    private int[] CreateOneRoute(){
+        //生成一条从起点到终点的路径
+
     }
 
     public void Crossover(){
--- code/future_net/src/com/routesearch/route/test.java
+++ code/future_net/src/com/routesearch/route/test.java
@@ -1,7 +1,22 @@
 package com.routesearch.route;
 
+import com.filetool.main.Main;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/3/12.
+ * 用来测试gitpush 和pull 成功与否
  */
 public class test {
+    private List []list;
+
+    public test(){
+
+    }
+    public  static  void main(String []args){
+
+    }
 }
+
